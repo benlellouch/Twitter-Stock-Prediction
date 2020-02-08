bad468d786c310a843c46d2f19d594381e08c6b3
BenWRees <br7g18@soton.ac.uk>
2020-02-08 23:41:13 +0000

    this mofo might be working now

--- stockPrediction.py
+++ stockPrediction.py
@@ -9,12 +9,16 @@ import datetime
 import math
 
 def stonks_bash(data_set) :
+	forecast_value = forecast_function(data_set)
+	return (forecast_value)*100
+	"""
 	if forecast_function(data_set) > 0:
 		return "Stock Increases"
 	elif forecast_function(data_set) < 0 :
 		return "Stock Deacreases"
 	else :
 		return "Stock stays the same"
+	"""
 
 """
 	forecasts the trend of the stock market by comparing the similarity in positive 
@@ -24,10 +28,10 @@ def forecast_function(data_set) :
 	positive_data_sum = 0
 	negative_data_sum = 0
 	for positive_data_value in positive_data(data_set) :
-		positive_data_sum = positive_data_sum + (time(positive_data_value)*SSM(positive_data(data_set),positive_data_value))
+		positive_data_sum = positive_data_sum + (time(positive_data_value)*SSM(data_set,[positive_data_value]))
 
 	for negative_data_value in negative_data(data_set) :
-		negative_data_sum = negative_data_sum + (time(negative_data_value)*SSM(negative_data(data_set),negative_data_value))
+		negative_data_sum = negative_data_sum + (time(negative_data_value)*SSM(data_set,[negative_data_value]))
 		
 	return positive_data_sum - negative_data_sum
 
@@ -68,8 +72,12 @@ def sum_of_x_squared(data_set) :
 	return x_squared_sum
 
 
-def mu_value(data_point_1,data_point_2) :
-	return 0 if (1-abs(data_point_1[0] - data_point_2[0])) < 0 else 1-abs(data_point_1[0] - data_point_2[0])
+def mu_value(data_value_1,data_value_2) :
+	#print(type(data_value_1), type(data_value_2))
+	if abs(data_value_1[0] - data_value_2[0]) >= 1 :
+		return 0
+	else :
+		return 1 - abs(data_value_1[0] - data_value_2[0])
 
 """
 	fuck python - haskell is better (basically the haskell take function)
@@ -84,7 +92,7 @@ def take(size, data_set) :
 	System Similarity Model 
 	data_set_1 has a cardinality of m, data_set_2 has a cardinality of n
 """
-def SSM(data_set_1, data_set_2) :
+def SSM(data_set_1, data_set_2) :	
 	p = len(data_set_1) if len(data_set_1) < len(data_set_2) else len(data_set_2)
 
 	#dealing with the square root of the summation of x^2 values
@@ -97,9 +105,13 @@ def SSM(data_set_1, data_set_2) :
 	x_squared_with_mu_sum = 0
 	for set_1_values in data_set_1_up_to_p :
 		for set_2_values in data_set_2_up_to_p :
-			x_squared_with_mu_sum = x_squared_with_mu_sum + (mu_value(set_1_values[0],set_2_values[0]) * (set_1_values[0])**2)
+			x_squared_with_mu_sum = x_squared_with_mu_sum + (mu_value(set_1_values,set_2_values) * (set_1_values[0])**2)
 
-
+	#dealing with summation of x^2_i * mu^2_i from 1 to p
+	x_squared_with_mu_squared_sum = 0
+	for set_1_values in data_set_1_up_to_p :
+		for set_2_values in data_set_2_up_to_p :
+			x_squared_with_mu_squared_sum = x_squared_with_mu_squared_sum + ((mu_value(set_1_values,set_2_values))**2 * (set_1_values[0])**2)
 
 	#dealing with the summation of y^2 from p+1 to n
 	y_squared_up_to_p_sum = 0
@@ -114,22 +126,31 @@ def SSM(data_set_1, data_set_2) :
 
 	y_squared_from_p_to_n = y_squared_sum - y_squared_up_to_p_sum
 
-	return x_squared_with_mu_sum / (x_sum * math.sqrt(x_squared_with_mu_sum + y_squared_from_p_to_n))
+	return x_squared_with_mu_sum / (math.sqrt(len(data_set_1)) * x_sum * math.sqrt(x_squared_with_mu_squared_sum + y_squared_from_p_to_n))
 
 	
 		
 		
 #Testing Area
-data_set = [(0.5413, datetime.datetime(2020, 2, 6, 16, 7, 46)), (-0.4939, datetime.datetime(2020, 2, 3, 16, 7, 43)), (0.8832, datetime.datetime(2020, 2, 8, 16, 7, 43)), (-0.34, datetime.datetime(2020, 2, 8, 16, 7, 42)), (0.7034, datetime.datetime(2020, 2, 8, 16, 7, 41)), (-0.0258, datetime.datetime(2020, 2, 8, 16, 7, 41)), (-0.1027, datetime.datetime(2020, 2, 8, 16, 7, 41)), (0.4588, datetime.datetime(2020, 2, 8, 16, 7, 41)), (0.4019, datetime.datetime(2020, 2, 8, 16, 7, 40))] 
+data_set = [(-1, datetime.datetime(2020, 2, 6, 16, 7, 46)),(-1, datetime.datetime(2020, 2, 6, 16, 7, 46)),(-1, datetime.datetime(2020, 2, 6, 16, 7, 46)),(-1, datetime.datetime(2020, 2, 6, 16, 7, 46)),(-1, datetime.datetime(2020, 2, 6, 16, 7, 46)),(-1, datetime.datetime(2020, 2, 6, 16, 7, 46)),(-1, datetime.datetime(2020, 2, 6, 16, 7, 46)),(-1, datetime.datetime(2020, 2, 6, 16, 7, 46)),(-1, datetime.datetime(2020, 2, 6, 16, 7, 46)), (-1, datetime.datetime(2020, 2, 6, 16, 7, 46)),(-1, datetime.datetime(2020, 2, 6, 16, 8, 46)), (-1, datetime.datetime(2020, 2, 3, 16, 7, 43)), (-1, datetime.datetime(2020, 2, 8, 16, 7, 43)), (-1, datetime.datetime(2020, 2, 8, 16, 7, 42)), (-1, datetime.datetime(2020, 2, 8, 16, 7, 41)), (-1, datetime.datetime(2020, 2, 8, 16, 7, 41)), (-1, datetime.datetime(2020, 2, 8, 16, 7, 41))] 
 data_set_1 = [(0.6369, datetime.datetime(2020, 2, 8, 16, 8, 10)), (0.2023, datetime.datetime(2020, 2, 8, 16, 8, 10)), (0.34, datetime.datetime(2020, 2, 8, 16, 8, 9)), (0.5106, datetime.datetime(2020, 2, 8, 16, 8, 9))]
 #print(negative_data(data_set))
 #print(sum_of_x_squared(data_set))
 positive_data_set = positive_data(data_set)
 negative_data_set = negative_data(data_set)
 
+"""
+print("SSM data set against positive data set:")
 print(SSM(data_set,positive_data_set))
+print("SSM positive data against whole data set:")
 print(SSM(positive_data_set,data_set))
+print("SSM data set against data set 1:")
 print(SSM(data_set,data_set_1))
-print(mu_value((0.5413, datetime.datetime(2020, 2, 6, 16, 7, 46)),(0.2023, datetime.datetime(2020, 2, 8, 16, 8, 10))))
+print("mu Value:")
+print(mu_value((0.4413, datetime.datetime(2020, 2, 6, 16, 7, 46)),(1.0000, datetime.datetime(2020, 2, 8, 16, 8, 10))))
+"""
+print("forecast: ",forecast_function(data_set_1), "%")
+
+#print(mu_value((0.5413, datetime.datetime(2020, 2, 6, 16, 7, 46)),(0.2023, datetime.datetime(2020, 2, 8, 16, 8, 10))))
 
 
