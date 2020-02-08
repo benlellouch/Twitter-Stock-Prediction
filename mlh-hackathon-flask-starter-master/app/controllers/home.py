544595dfc108ce3184679c6d5f5ea8fd1fde4257
Behrad Koohy <behradkoohy@Behrad-Koohys-MacBook-Pro-2.local>
2020-02-08 17:35:33 +0000

    Basic stuff for the python side of homepage done

--- mlh-hackathon-flask-starter-master/app/controllers/home.py
+++ mlh-hackathon-flask-starter-master/app/controllers/home.py
@@ -1,8 +1,38 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify, request
+from yahoo_fin import stock_info as sf
+
 
 blueprint = Blueprint('home', __name__)
 
-@blueprint.route('/')
+
+def get_score(ticker):
+	if ticker == 'AAPL': return 0.63
+
+
+def is_positive(score):
+	if score > 0: return True
+	else: return False
+
+
+
+
+@blueprint.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('home/index.html')
+	# if request.method == 'GET':
+	# 	pass
+	# if request.method == 'POST':
+	# 	# get the form details
+	if True:
+		selected_stock_ticker = 'AAPL'
+
+		# score
+		score = get_score(selected_stock_ticker)
+		# Feedback
+		feedback = is_positive(score)
+		# Stock price
+		ticker_price = sf.get_live_price("AAPL")
+		return jsonify([score, feedback, ticker_price])
+		return render_template('home/index.html')
+
+
