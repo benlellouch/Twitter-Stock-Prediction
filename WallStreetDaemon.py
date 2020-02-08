5e47301fdb1898aad9a40f79533c0fc35866a378
Sovereign757 <isrealgenius@gmail.com>
2020-02-08 18:01:14 +0000

    Add files via upload
    Including DaemonThread

--- WallStreetDaemon.py
+++ WallStreetDaemon.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import yfinance as yf
 from yahoo_fin import stock_info as si
-
+from datetime import datetime
+import time
+import threading
 # requires panda, yfinance and yahoo-fin, sometimes listed as yahoo-fin
 # all dates in format of 2020-01-05
 # all company names in format of TSLA
 # all arguments inputed as strings
-#all prices in USD
-
+# all prices in USD
+companyList= ["TSLA","AAPL"]
 # use getStockInfo to create data frame df
 def getStockInfo(company,startDate, endDate):
     data = yf.download(company, start=startDate, end=endDate)
@@ -20,5 +22,18 @@ def getMarketHiLo(df, date):
     return hiLo # outputted as a tuple
 
 def getCurrentPrice(company):
-    si.get_live_price(company) # numerical output
+    return si.get_live_price(company) # numerical output
 
+def daemonWolfy(companyList):
+    pastList = []
+    for i in range(0, len(companyList)):
+        pastList.append(list(companyList[i]))
+    while True:
+        for i in range(0,len(companyList)):
+            pastList[i].append((getCurrentPrice(companyList[i]), str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))))
+            print(companyList[i],getCurrentPrice(str(companyList[i])))
+        time.sleep(60)
+
+
+theWolfAmongUs = threading.Thread(target=daemonWolfy, args=(companyList,), daemon=True)
+theWolfAmongUs.start()
