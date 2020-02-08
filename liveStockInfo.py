68c774d0bcb46d7494dc1f3b0809dd6dfd135f01
BenWRees <br7g18@soton.ac.uk>
2020-02-08 15:43:22 +0000

    I don't even know what's going on with this class anymore

--- liveStockInfo.py
+++ liveStockInfo.py
@@ -2,4 +2,18 @@
 	This class deals with all the live stock info coming in
 
 """
-class LiveStockInfo :
+import threading 
+import pandas as pd
+import pandas_datareader
+class Live_Stock_Info :
+
+
+	def 
+
+
+	"""
+		inner class that runs a daemon thread of the stock info 
+	"""
+	class Live_Stock_Thread :
+		def __init__ (self) :
+			threading.Thread(target = , args = ,daemon = True)
