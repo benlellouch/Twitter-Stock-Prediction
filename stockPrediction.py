355bb732fe7ba5081ec130d8113977c05d5c2ba1
Benjamin Rees <br7g18@soton.ac.uk>
2020-02-08 12:07:50 +0000

    initial commit for the stock prediction

---
+++ stockPrediction.py
@@ -0,0 +1,8 @@
+"""
+	This class deals with taking a data set of sentiment analysis on a company, at any one time and then calculating the change of stock price based on that sentiment
+
+	Have a seperate thread running in the background dealing with a company's live stock price that we can get data from to apply to the stock price change 
+
+	Produce values of the predicted stock price based on the sentiment analysis
+"""
+class 
