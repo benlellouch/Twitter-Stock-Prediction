dee37eec00cb5b74c83dda4cac9d97d50914171f
Benjamin Lellouch <blellouch@gmail.com>
2020-02-08 20:22:25 +0000

    added an average function that averages the score of a batch of tweets

--- sentimentAnalyser.py
+++ sentimentAnalyser.py
@@ -64,3 +64,20 @@ class SentimentAnalyser:
 
 
         return analysed_tweets
+
+    def average_score(self, analysed_tweets):
+
+        cumsum = 0
+        for tweet in analysed_tweets:
+            cumsum += tweet[0]
+        return (cumsum/len(analysed_tweets))
+
+def main():
+    tf = TwitterFetcher()
+    sa = SentimentAnalyser()
+    tweets = sa.analyse(tf.get_tweets("Google",100))
+    score  = sa.average_score(tweets)
+    print (score)
+
+if __name__ == "__main__":
+    main()
