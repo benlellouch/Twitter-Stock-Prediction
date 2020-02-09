0980f06daacf11dcf06842a4761b4c4344718a93
onryo <jda.dalessio98@gmail.com>
2020-02-09 08:03:09 +0000

    front end now displays prediction, score and plot

--- mlh-hackathon-flask-starter-master/app/controllers/home.py
+++ mlh-hackathon-flask-starter-master/app/controllers/home.py
@@ -10,48 +10,64 @@ blueprint = Blueprint('home', __name__)
 
 
 def get_score(ticker):
-	if ticker == 'AAPL':
-		return 0.63
-	else:
-		return 0.45
+    with open("../date&score-pairs.json", "r") as f:
+        data = json.loads(f.read())
+    key = max(data.keys())
+    for dict in (data[key]):
+        if(dict.get(ticker)):
+            return dict.get(ticker)
 
 
+def get_prediction(ticker):
+    with open("../predictions.json", "r") as f:
+        data = json.loads(f.read())
+    key = max(data.keys())
+    for dict in (data[key]):
+        if(dict.get(ticker)):
+            return dict.get(ticker)
 
 
 def calculate_accuracy(ticker):
-	with open('../accuracy.json') as f:
-		data = json.loads(f.read())
-	return data[ticker]
+    with open('../accuracy.json') as f:
+        data = json.loads(f.read())
+    return data[ticker]
+
+
+def get_image(ticker):
+    image_path = "static/" + ticker + ".png"
+    return image_path
 
 
 @blueprint.route('/', methods=['GET', 'POST'])
 def index():
-	print('index')
-	selected_stock_ticker = 'AAPL'
+    print('index')
+    selected_stock_ticker = 'AAPL'
 
-	if request.method == 'GET':
-		print('GET')
+    if request.method == 'GET':
+        print('GET')
 
-	if request.method == 'POST':
-		print('dasd')
-		ticker = (str(request.form['selected']).strip()[-16::]).strip()
-		print(ticker)
-		selected_stock_ticker = ticker
+    if request.method == 'POST':
+        print('dasd')
+        ticker = (str(request.form['selected']).strip()[-16::]).strip()
+        print(ticker)
+        selected_stock_ticker = ticker
 
-	company_data = {}
-	selected_data = {}
+    company_data = {}
+    selected_data = {}
 
+    with open('../data.json') as f:
+        company_data = json.loads(f.read())
 
-	with open('../data.json') as f:
-		company_data = json.loads(f.read())
+    selected_data['ticker'] = selected_stock_ticker
+    selected_data['name'] = company_data[selected_stock_ticker]
+    selected_data['price'] = '%.2f' % (
+        sf.get_live_price(selected_stock_ticker))
+    # selected_data['open'] = '%.2f' % (sf.get_open(selected_stock_ticker))
+    selected_data['score'] = get_score(selected_stock_ticker)
+    selected_data['accuracy'] = '%.2f' % calculate_accuracy(
+        selected_stock_ticker)
+    selected_data['tweets'] = ""
+    selected_data['prediction'] = get_prediction(selected_stock_ticker)
+    selected_data['plot'] = get_image(selected_stock_ticker)
 
-	selected_data['ticker'] = selected_stock_ticker
-	selected_data['name'] = company_data[selected_stock_ticker]
-	selected_data['price'] = '%.2f' % (sf.get_live_price(selected_stock_ticker))
-	# selected_data['open'] = '%.2f' % (sf.get_open(selected_stock_ticker))
-	selected_data['score'] = get_score(selected_stock_ticker)
-	selected_data['accuracy'] = '%.2f' % calculate_accuracy(selected_stock_ticker)
-	selected_data['tweets'] = ""
-
-
-	return render_template('home/index.html', stonks=company_data, data=selected_data)
+    return render_template('home/index.html', stonks=company_data, data=selected_data)
--- plotter.py
+++ plotter.py
@@ -3,23 +3,25 @@ import matplotlib.pyplot as plt
 import os
 import json
 
+
 def generate_graph(f, ticker):
     if not os.path.exists("plots"):
         os.mkdir("plots")
     x_plot = []
     y_plot = []
-    file = open(f,"r").read()
+    file = open(f, "r").read()
     json_file = json.loads(file)
-    for (k,list_dicts) in zip(json_file.keys(), json_file.values()):
+    for (k, list_dicts) in zip(json_file.keys(), json_file.values()):
         for dict in list_dicts:
             if (dict.get(ticker)):
                 x_plot.append(k)
                 y_plot.append(dict.get(ticker))
     fig = plt.figure()
     ax = plt.subplot(111)
-    ax.plot(x_plot,y_plot)
+    ax.plot(x_plot, y_plot)
     plt.title("Average Popularity Score of " + ticker)
     plt.xlabel("Epoch Time")
     plt.ylabel("Average Popularity Score")
 
-    fig.savefig("plots/" + ticker + ".png")
+    fig.savefig(
+        "mlh-hackathon-flask-starter-master/app/static/" + ticker + ".png")
