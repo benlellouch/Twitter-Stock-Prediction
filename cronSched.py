a13a7e29d9526a1e2eb49be66c8aab2237f47304
Michael Gimson <56403978+MichaelGimson@users.noreply.github.com>
2020-02-09 02:25:19 +0000

    added randomdata(f) function
    outputs json file "dummydata" which has timestamp followed by acronym-int pairings
    APPL : 49

--- cronSched.py
+++ cronSched.py
@@ -4,6 +4,8 @@ import nltk
 nltk.download('vader_lexicon')
 from sentimentAnalyser import TwitterFetcher, SentimentAnalyser
 from random import random
+from random import seed
+from random import randint
 import time
 
 DEBUG = False
@@ -22,13 +24,14 @@ def querySentiment(f):
             time_dict = json.loads(f.read())
             time_dict[current_time] = []
     except Exception:
-        time_dict = {current_time : []}
+        time_dict = {current_time: []}
     for k in company_data:
         # gets tweets -> tweets analysed
         analysed = sa.analyse(tf.get_tweets(company_data[k], 100))
         if analysed:
+        # if True:
             if not DEBUG:
-                #  -> average score calculated
+                # average score calculated
                 avg = sa.average_score(analysed)
             else:
                 avg = random()
@@ -40,6 +43,19 @@ def querySentiment(f):
         json.dump(time_dict, outfile)
 
 
+def randomdata(f):
+    current_time = time.time()
+    dummy_data = {}
+    # dictionary of acronym : name
+    company_data = json.loads(f.read())
+    seed(1)
+    dummy_data[current_time] = []
+    for k in company_data:
+        value = randint(45,70)
+        dummy_data[current_time].append({k:value})
+    with open("dummydata.json", "w") as outfile:
+        json.dump(dummy_data, outfile)
+
 # funky way of being able to serialize datetime
 def myconverter(o):
     if isinstance(o, datetime.datetime):
@@ -47,6 +63,7 @@ def myconverter(o):
 
 
 # opens json data file
-if __name__ == '__main__':
+def main():
     with open('data.json') as file:
+        randomdata(file)
         querySentiment(file)
