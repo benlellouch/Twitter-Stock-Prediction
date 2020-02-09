32f79c65d3541312f4d60823f8ca63814b6e144b
Benjamin Lellouch <blellouch@gmail.com>
2020-02-09 04:49:56 +0000

    Addind Ben Rees' sick stock prediction algorithm

--- cronSched.py
+++ cronSched.py
@@ -3,6 +3,7 @@ import json
 import nltk
 # nltk.download('vader_lexicon')
 from sentimentAnalyser import TwitterFetcher, SentimentAnalyser
+from stockPrediction import StockPrediction
 from plotter import generate_graph
 from random import random
 from random import seed
@@ -13,8 +14,10 @@ DEBUG = False
 def querySentiment(f):
     tf = TwitterFetcher()
     sa = SentimentAnalyser()
+    sp = StockPrediction()
     current_time = time.time()
     time_dict = {}
+    pred_dict = {}
     # dictionary of acronym : name
     company_data = json.loads(f.read())
     try:
@@ -23,11 +26,18 @@ def querySentiment(f):
             time_dict[current_time] = []
     except Exception:
         time_dict = {current_time: []}
+    try:
+        with open("predictions.json", "r") as f:
+            pred_dict = json.loads(f.read())
+            pred_dict[current_time] = []
+    except Exception:
+        pred_dict = {current_time: []}
     for k in company_data:
         # gets tweets -> tweets analysed
         analysed = sa.analyse(tf.get_tweets(company_data[k], 100))
         if analysed:
         # if True:
+            prediction = sp.stonks_bash(analysed)
             if not DEBUG:
                 # average score calculated
                 avg = sa.average_score(analysed)
@@ -36,9 +46,13 @@ def querySentiment(f):
             print(company_data[k])
             # time : {company, average score}
         time_dict[current_time].append({k:avg})
+        pred_dict[current_time].append({k:prediction})
     print(time_dict)
+    print(pred_dict)
     with open("date&score-pairs.json", "w") as outfile:
         json.dump(time_dict, outfile)
+    with open("predictions.json", "w") as outfile:
+        json.dump(pred_dict, outfile)
     for k in company_data:
         generate_graph("date&score-pairs.json", k)
 
@@ -66,3 +80,6 @@ def myconverter(o):
 def main():
     with open("data.json", "r") as file:
         querySentiment(file)
+
+if __name__ == "__main__":
+    main()
