488ab81643a1c543944daf541b4bb9d53d6606b5
onryo <jda.dalessio98@gmail.com>
2020-02-09 00:01:43 +0000

    front end makes post request and all

--- mlh-hackathon-flask-starter-master/app/controllers/home.py
+++ mlh-hackathon-flask-starter-master/app/controllers/home.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template, jsonify, request
 from yahoo_fin import stock_info as sf
-import json 
+import json
 import os
 
 
@@ -9,43 +9,40 @@ blueprint = Blueprint('home', __name__)
 
 
 def get_score(ticker):
-	if ticker == 'AAPL': return 0.63
+    if ticker == 'AAPL':
+        return 0.63
 
 
 def is_positive(score):
-	if score > 0: return True
-	else: return False
-
-
+    if score > 0:
+        return True
+    else:
+        return False
 
 
 @blueprint.route('/', methods=['GET', 'POST'])
 def index():
-	# if request.method == 'GET':
-	# 	show = False
+    # if request.method == 'GET':
+    # 	show = False
 
-	# if request.method == 'POST':
-	# 	show = True
+    if request.method == 'POST':
+        # 	show = True
+        return jsonify(request.form)
 
-	company_data = {}
-	print(os.getcwd())
-	with open('../data.json') as f:
-		company_data = json.loads(f.read())
+    company_data = {}
+    print(os.getcwd())
+    with open('../data.json') as f:
+        company_data = json.loads(f.read())
 
+    if True:
 
-	if True:
+        selected_stock_ticker = 'AAPL'
 
-
-
-		selected_stock_ticker = 'AAPL'
-
-		# score
-		score = get_score(selected_stock_ticker)
-		# Feedback
-		feedback = is_positive(score)
-		# Stock price
-		ticker_price = (sf.get_live_price("AAPL"))
-		datum = [score, feedback, '%.2f' % ticker_price]
-		return render_template('home/index.html', stonks=company_data, data=datum)
-
-
+        # score
+        score = get_score(selected_stock_ticker)
+        # Feedback
+        feedback = is_positive(score)
+        # Stock price
+        ticker_price = (sf.get_live_price("AAPL"))
+        datum = [score, feedback, '%.2f' % ticker_price]
+        return render_template('home/index.html', stonks=company_data, data=datum)
